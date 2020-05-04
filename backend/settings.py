1e2944fd986c5ca63cb7e9c4821bfc745b3b1fff
ngade98 <ngade98@gmail.com>
2020-05-03 20:58:21 -0500

    added tutorial changes

--- backend/settings.py
+++ backend/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = True #need to change with production
 
